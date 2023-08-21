0ec0af2f504c045618b6a435aba5418f4d5ff6a3
luka@taylor-corp.net <luka@Lemondo.biz>
2023-08-21 17:25:50 +0400

    Done all 4 units

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { FormatDataComponent } from './format-data/format-data.component';
-import {FormatDatePipe} from "../shared/format-data.pipe";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {TextComparisonComponent} from "./text-comparision/text-comparision.component";
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalendarComponent,
-    FormatDataComponent,
-    FormatDatePipe,
-    TextComparisonComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
