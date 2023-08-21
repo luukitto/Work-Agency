aba971025b81e9a278c916f99e6ce39344b900b2
luka@taylor-corp.net <luka@Lemondo.biz>
2023-08-21 17:21:45 +0400

    Done all 4 units

---
+++ src/app/format-data/format-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-format-data',
+  templateUrl: './format-data.component.html',
+  styleUrls: ['./format-data.component.css']
+})
+export class FormatDataComponent {
+
+}
